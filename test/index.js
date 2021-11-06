7fab6b4acef703ece53395c940c5c7d0ff77e6ff
Lily Smart <lily@neyoa.me>
2021-11-06 16:58:35 +0000

    Create a few tests

--- test/index.js
+++ test/index.js
@@ -1,14 +1,92 @@
-const { createBot } = require('mineflayer')
-const hyflayer = require('..')
+const mineflayer = require('mineflayer')
+const hyflayer = require('hyflayer')
+const mc = require('minecraft-protocol')
+const assert = require('assert')
 
-/**
- * @type {import('..').HyflayerBot}
- */
-const bot = createBot()
+/** @type {hyflayer.HyflayerBot} */
+const bot = mineflayer.createBot()
 bot.loadPlugin(hyflayer)
 
+/**
+ * @param {mc.Client} client
+ * @param {string} message
+ */
+function chat(client, message) {
+  client.write('chat', { message, position: 0, sender: 0 })
+}
+
+for (const version of ['1.17.1'] /* mineflayer.testedVersions */) {
+  describe(`Internal tests - ${version}`, () => {
+    const username = 'hyflayer'
+    const port = 25567
+
+    describe('Chat message detection', () => {
+      /** @type {hyflayer.HyflayerBot} */
+      let bot
+      /** @type {mc.Server} */
+      let server
+
+      beforeEach((done) => {
+        server = mc.createServer({
+          'online-mode': false,
+          version,
+          port,
+        })
+
+        server.on('listening', () => {
+          bot = mineflayer.createBot({
+            username,
+            version,
+            port,
+          })
+          bot.loadPlugin(hyflayer)
+          done()
+        })
+      })
+      afterEach((done) => {
+        bot.on('end', done)
+        server.close()
+      })
+
+      it('Tests guild chat', (done) => {
+        server.on('login', (client) => {
+          const tests = [
+            {
+              description: 'Hypixel and Guild rank',
+              message: 'Guild > [MVP+] neyoa [Staff]: message',
+              expected: { message: 'message', hypixelRank: '[MVP+]', guildRank: '[Staff]', username: 'neyoa' },
+            },
+            {
+              description: 'Guild rank only',
+              message: 'Guild > neyoa [Staff]: message',
+              expected: { message: 'message', hypixelRank: null, guildRank: '[Staff]', username: 'neyoa' },
+            },
+            {
+              description: 'Hypixel and Guild rank',
+              message: 'Guild > [MVP+] neyoa: message',
+              expected: { message: 'message', hypixelRank: '[MVP+]', guildRank: null, username: 'neyoa' },
+            },
+            {
+              description: 'No ranks',
+              message: 'Guild > neyoa: message',
+              expected: { message: 'message', hypixelRank: null, guildRank: null, username: 'neyoa' },
+            },
+          ]
+          for (const { description, message, expected } of tests) {
+            it(`Tests ${description} message`)
+            chat(client, message)
+
+            bot.on('guildChatMessage', (message, { hypixelRank, guildRank, username }) => {
+              test({ message, hypixelRank, guildRank, username }, expected)
+            })
+          }
+        })
+      })
+    })
+  })
+}
+
 const testMessages = [
-  'Guild > [MVP+] neyoa [Staff]: message',
   'Guild > neyoa [Staff]: message',
   'Guild > [MVP+] neyoa: message',
   'Guild > neyoa: message',
@@ -38,30 +116,4 @@ bot.once('spawn', () => {
   bot.sendToOfficer('officer', 'prefix: ')
   bot.sendToParty('party')
   bot.sendToParty('party', 'prefix: ')
-})
-
-bot.on('guildChatMessage', (message, { hypixelRank, username, guildRank }, messageJson) => {
-  const prefix = [username]
-  hypixelRank && prefix.unshift(hypixelRank)
-  guildRank && prefix.push(guildRank)
-  console.log('Guild -', `${prefix.join(' ')}:`, message)
-})
-
-bot.on('partyChatMessage', (message, { hypixelRank, username }, messageJson) => {
-  const prefix = [username]
-  hypixelRank && prefix.unshift(hypixelRank)
-  console.log('Party -', `${prefix.join(' ')}:`, message)
-})
-
-bot.on('officerChatMessage', (message, { hypixelRank, username, guildRank }, messageJson) => {
-  const prefix = [username]
-  hypixelRank && prefix.unshift(hypixelRank)
-  guildRank && prefix.push(guildRank)
-  console.log('Officer -', `${prefix.join(' ')}:`, message)
-})
-
-bot.on('allChatMessage', (message, { hypixelRank, username }, messageJson) => {
-  const prefix = [username]
-  hypixelRank && prefix.unshift(hypixelRank)
-  console.log('All -', `${prefix.join(' ')}:`, message)
 })
